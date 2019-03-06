2a5b84e8ec777d9b3b978b0f9896680a439b3536
LAPASSET Alexis <LAPASSAL@CDM.CM-CIC.FR>
2019-03-06 15:35:59 +0100

    feat(App): ajoute la structure en deux colonnes de l'application

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
 
 export default class App extends React.Component {
   render () {
-    return <h1>Hello World</h1>
+    return <div id='mowitnow'>
+      <h1>Mow It Now</h1>
+      <Grid container spacing={10}>
+        <Grid item xs={6}>
+          <h2>Jardin</h2>
+        </Grid>
+        <Grid item xs={6}>
+          <h2>Contrôles</h2>
+        </Grid>
+      </Grid>
+    </div>
   }
 }
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import Grid from '@material-ui/core/Grid'
 import App from './App'
 
 describe('Given I\'m on Mow It Now main page', () => {
@@ -9,8 +10,20 @@ describe('Given I\'m on Mow It Now main page', () => {
   })
 
   describe('When the page is loaded', () => {
-    test('Then the "Hello World" is shown', () => {
-      expect(shallow(<App />).contains(<h1>Hello World</h1>)).toBe(true)
+    test('Then the grid with two columns Jardin & Contrôles is shown', () => {
+      expect(shallow(<App />).contains(
+        <div id='mowitnow'>
+          <h1>Mow It Now</h1>
+          <Grid container spacing={10}>
+            <Grid item xs={6}>
+              <h2>Jardin</h2>
+            </Grid>
+            <Grid item xs={6}>
+              <h2>Contrôles</h2>
+            </Grid>
+          </Grid>
+        </div>
+      )).toBe(true)
     })
   })
 })
